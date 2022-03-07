011e8d044b44f66e0d7124aa09f2379467654647
Bruno Oliveira <bruno.bx04@gmail.com>
2022-03-07 17:33:50 +0000

    Start Menu Function
    Implementation:
    -Add Menu Function to organize Code

--- TrabalhoPratico_EDA/TrabalhoPratico_EDA/Source_Code.c
+++ TrabalhoPratico_EDA/TrabalhoPratico_EDA/Source_Code.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#include <locale.h>
+#include <time.h>
+#include <wchar.h>
 #include <stdlib.h>
+#include "Functions.h"
 
 int main() {
 
+	setlocale(LC_ALL, "portuguese");
+	Menu();
 
-	printf("Hello Word");
+
 	return 0;
+
+}
+
+void Menu(){
+
+
+	printf("MENU");
+
+
 
 }
